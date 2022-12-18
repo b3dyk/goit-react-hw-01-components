20d4c2990a87fb264601cfb3d26c8fae8152f8e0
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-12-18 04:12:34 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -16,7 +16,7 @@ function getRandomColor() {
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={classNames(styles.statistics)}>
-      {title ? <h2 className={classNames(styles.title)}>{title}</h2> : <></>}
+      {title && <h2 className={classNames(styles.title)}>{title}</h2>}
 
       <ul className={classNames(styles.statList)}>
         {stats.map(({ id, label, percentage }) => {
