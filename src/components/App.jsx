044832783c9330eccc66d67c3223e0e383482df3
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-12-18 03:00:40 +0200

    fix App styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
+
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
@@ -13,6 +14,7 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
